7c0942170c16a948ab961dea84fcb9e16c9ad25a
Javier <javierohern@me.com>
2023-10-09 15:43:09 -0700

    My gradient solution

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ container.style.display = "flex";
 container.style.flexWrap = "wrap";
 container.style.width = "800px";
 container.style.height = "800px";
+container.style.background = "linear-gradient(135deg, blue, green)";
 
 for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
@@ -11,15 +12,12 @@ for (let i = 0; i < 8; i++) {
 
     tile.style.width = "12.5%";
     tile.style.height = "12.5%";
-    tile.style.backgroundColor = getRandomColor();
+    if ((i+j) % 2 === 0) {
+        tile.style.background = "rgba(0, 0, 255," + (.02 * i) + ")";
+    } else {
+        tile.style.background = "rgba(0, 102, 0," + (.02 * i) + ")";
+    }
     container.appendChild(tile);
   }
 }
 document.body.append(container);
-
-function getRandomColor() {
-    let r = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${b}, ${g})`;
-}
